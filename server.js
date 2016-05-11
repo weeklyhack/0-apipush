c84c4c6240ce0502225fa46472c643d2aba34105
1egoman <rsg1egoman@gmail.com>
2016-05-11 18:19:54 -0400

    handle all possible routes

--- server.js
+++ server.js
@@ -21,29 +21,47 @@ const data = {
         },
       ],
     },
-  }
+  },
 };
 
-data.versions.v1.routes.forEach(i => {
-  app[i.accept.method.toLowerCase()](i.accept.url, (req, res) => {
+function stripInitialSlash(data) {
+  if (data[0] === '/') {
+    return data.slice(1);
+  } else {
+    return data;
+  }
+}
 
-    // send the data to the client's system
-    i.proxy.forEach(data => {
-      // take the proxy route and render each key
-      let dataRender = Object.assign({}, data);
-      for (let item in dataRender) {
-        if (dataRender.hasOwnProperty(item)) {
-          dataRender[item] = Mustache.render(dataRender[item], req);
-        }
-      }
-      console.log(dataRender);
-
-      // make the request
-      let out = request(dataRender).on('error', err => {
-        res.json({error: err.toString()});
-      }).pipe(res);
+app.all("/api/:version/*", (req, res) => {
+  let version;
+  if (version = data.versions[req.params.version]) {
+    let route = version.routes.find(i => {
+      return i.accept.method === req.method && 
+      `/api/${req.params.version}/${stripInitialSlash(i.accept.url)}` === req.url;
     });
-  });
+
+    if (route && route.proxy.length) {
+      // send the data to the client's system
+      (function(data) {
+        // take the proxy route and render each key
+        let dataRender = Object.assign({}, data);
+        for (let item in dataRender) {
+          if (dataRender.hasOwnProperty(item)) {
+            dataRender[item] = Mustache.render(dataRender[item], req);
+          }
+        }
+
+        // make the request
+        let out = request(dataRender).on('error', err => {
+          res.json({error: err.toString()});
+        }).pipe(res);
+      })(route.proxy[0]);
+    } else {
+      res.status(400).json({error: "No such route, or route cannot handle the request"});
+    }
+  } else {
+    res.status(400).json({error: `Version ${req.params.version} isn't valid.`});
+  }
 });
 
 app.listen(process.env.PORT || 8000);
