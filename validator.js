addc5e3f4cba846cf5300513f34cf1d5eaf4a8e0
1egoman <rsg1egoman@gmail.com>
2016-05-14 17:51:05 -0400

    add json validator for an api

---
+++ validator.js
@@ -0,0 +1,101 @@
+import {Validator} from "jsonschema";
+import urlRegex from "url-regex";
+
+Validator.prototype.customFormats.url = function url(input) {
+  return urlRegex({exact: true}).test(input);
+}
+
+var v = new Validator();
+
+let httpMethodTypes = ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"];
+
+let proxyHttpSchema = {
+  type: "object",
+  properties: {
+    via: {type: {enum: ["http"]}},
+    url: { type: "string", format: "url" },
+    method: {type: {enum: httpMethodTypes}},
+    headers: {type: "string"},
+    body: {type: "string"},
+  },
+  required: ["via", "url", "method", "headers", "body"],
+};
+
+let proxyWebsocketsSchema = {
+  type: "object",
+  properties: {
+    via: {type: {enum: ["websockets"]}},
+    url: { type: "string", format: "url" },
+    send: {
+      type: "array",
+      items: { type: "string" },
+    },
+    responses: {
+      type: "object",
+      patternProperties: {
+        "^[a-zA-Z0-9_]+$": {
+          type: "object",
+          properties: {
+            contains: {type: "string"},
+            then: {type: "string"},
+          },
+          required: ["contains", "then"],
+        },
+      },
+    },
+  },
+  required: ["via", "url", "send", "responses"],
+};
+
+let schema = {
+  type: "object",
+  properties: {
+    lines: {
+      type: "array",
+      items: {"type": "string"}
+    },
+    name: {type: "string"},
+    slug: {type: "string"},
+    desc: {type: "string"},
+    icon: {type: "string"},
+    versions: {
+      type: "object",
+      patternProperties: {
+        "^[a-zA-Z0-9_]+$": {
+          type: "object",
+          properties: {
+            routes: {
+              type: "array",
+              items: {
+                type: "object",
+                properties: {
+                  accept: {
+                    type: "object",
+                    properties: {
+                      method: {type: {enum: httpMethodTypes}},
+                      url: { type: "string" },
+                    },
+                  },
+                  proxy: {
+                    type: "array",
+                    items: {
+                      type: "object",
+                      oneOf: [proxyHttpSchema, proxyWebsocketsSchema],
+                    },
+                  },
+                },
+                required: ["accept", "proxy"],
+              },
+            },
+            required: ["routes"],
+          },
+        },
+      },
+    },
+  },
+  required: ["name", "slug"],
+};
+
+export default function validate(instance) {
+  return v.validate(instance, schema);
+};
