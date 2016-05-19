2d3c89f944ac5397758dc65047a3a21a85e4f74e
1egoman <rsg1egoman@gmail.com>
2016-05-19 17:29:49 -0400

    add examples and upsert apis on create

--- apis.js
+++ apis.js
@@ -7,82 +7,10 @@ import validate from "./validator";
 import _ from "lodash";
 import mongoose from 'mongoose';
 
-let data = [
-  {
-    name: "My Cool API",
-    slug: "coolapi",
-    desc: "Doing coolness since a while ago.",
-    createdBy: "1",
-    icon: "http://placehold.it/300x300",
-    versions: {
-      v1: {
-        routes: [
-          {
-            id: "beb18f57-217d-481f-a127-ead22feeca41",
-            accept: {
-              method: "GET",
-              url: "/devices",
-            },
-            proxy: {
-              // {
-              //   method: "GET",
-              //   url: "http://scooterlabs.com/echo?get=devices",
-              // }
-              id: "376882b1-f0e7-4393-886d-2aa93d357ff7",
-              via: "websockets",
-              url: "ws://echo.websocket.org",
-              send: [
-                `{"type": "UPDATE", "content": "{{query.value}}"}`,
-              ],
-              responses: {
-                success: {
-                  contains: "success",
-                  then: `{"response": true}`,
-                },
-                fail: {
-                  contains: "fail",
-                  then: `{"response": false}`,
-                },
-              },
-            }
-          },
-          {
-            id: "de9147c5-6290-4fec-9cbb-9edcf3235669",
-            accept: {
-              method: "GET",
-              url: "/devices/:id",
-            },
-            proxy: {
-              id: "75d39208-cb16-413f-890f-b85a8a87aa7d",
-              via: "http",
-              method: "POST",
-              url: "http://scooterlabs.com/echo?q={{params.id}}",
-              headers: "Test: {{params.id}}",
-              body: "abc",
-            },
-          },
-          {
-            id: "21e17c70-5e57-4acb-a19e-eb9a807f8290",
-            accept: {
-              method: "GET",
-              url: "/user/:uid",
-            },
-            proxy: {
-              id: "0291ee49-685e-4e76-8eb4-d50a844d0336",
-              via: "http",
-              method: "GET",
-              url: "http://randomuser.me/api/{{query.user}}",
-              body: "",
-              headers: "",
-            },
-          }
-        ],
-      },
-    },
-  },
-];
-
-const ApiModel = mongoose.model('Api', { model: Object });
+const ApiModel = mongoose.model('Api', {
+  model: Object,
+  slug: String,
+});
 
 function getSlugVersion(slug, version) {
   let slugData = data.find(i => i.slug === slug);
@@ -135,7 +63,13 @@ function findAll() {
 
 function findWithSlug(slug) {
   if (slug) {
-    return ApiModel.find({model: {slug}});
+    return ApiModel.findOne({slug}).then(api => {
+      if (api) {
+        return api.model;
+      } else {
+        return null;
+      }
+    });
   } else {
     return Promise.resolve(null);
   }
@@ -165,8 +99,8 @@ function create(api, user) {
         // validate the api to make sure it matches our expectations
         let validation = validate(api);
         if (validation.errors.length === 0) {
-          data.push(api);
-          return new ApiModel({model: api}).save();
+          // upsert the model
+          return ApiModel.update({slug: api.slug}, {model: api, slug: api.slug}, {upsert: true});
         } else {
           throw new VisibleError(400, `Schema Validation Errors: ${validation.errors.toString()}`);
         }
